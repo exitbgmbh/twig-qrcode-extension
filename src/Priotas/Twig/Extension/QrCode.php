3d8193b40c87cc4b1e9696df944c2693a964363e
roman <roman.robel@exitb.de>
2026-02-20 00:06:15 +0100

    fix

--- src/Priotas/Twig/Extension/QrCode.php
+++ src/Priotas/Twig/Extension/QrCode.php
@@ -3,7 +3,17 @@
 namespace Priotas\Twig\Extension;
 
 use Endroid\QrCode\Exception\ValidationException;
-use Endroid\QrCode\Factory\QrCodeFactory;
+use Endroid\QrCode\Label\Font\Font;
+use Endroid\QrCode\Label\Label;
+use Endroid\QrCode\Logo\Logo;
+use Endroid\QrCode\QrCode as EndroidQrCode;
+use Endroid\QrCode\RoundBlockSizeMode\RoundBlockSizeModeShrink;
+use Endroid\QrCode\Writer\BinaryWriter;
+use Endroid\QrCode\Writer\DebugWriter;
+use Endroid\QrCode\Writer\EpsWriter;
+use Endroid\QrCode\Writer\PdfWriter;
+use Endroid\QrCode\Writer\PngWriter;
+use Endroid\QrCode\Writer\SvgWriter;
 use Exception;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
@@ -34,33 +44,52 @@ class QrCode extends AbstractExtension
         $type = self::DEFAULT_TYPE,
         $size = self::DEFAULT_IMAGE_SIZE,
         $label = '',
-        $version = null,
-        $svg = self::SVG_OUTPUT_DATA_URI
+        $labelFontPath = '',
+        $margin = 0,
+        $logo = '',
+        $logoResizeToWidth = null,
+        $logoResizeToHeight = null
     ) {
-        $options = [
-            'writer' => $type,
-            'label' => $label,
-            'size' => $size
-        ];
-
-        $factory = new QrCodeFactory();
-        $qrCode = $factory->create($text, $options);
-
-        if ($type !== 'svg') {
-            return $qrCode->writeDataUri();
-        }
-
-        switch ($svg) {
-            case self::SVG_OUTPUT_DATA_URI:
-                return $qrCode->writeDataUri();
-            case self::SVG_OUTPUT_INLINE:
-                $text = $qrCode->writeString();
-                return substr($text, strpos($text, "\n") + 1);
+        switch ($type) {
+            case 'svg':
+                $writer = new SvgWriter();
+                break;
+            case 'eps':
+                $writer = new EpsWriter();
+                break;
+            case 'pdf':
+                $writer = new PdfWriter();
+                break;
+            case 'binary':
+                $writer = new BinaryWriter();
+                break;
             default:
-                throw new \InvalidArgumentException(
-                    sprintf('The svg options can either be "%s" or "%s"', self::SVG_OUTPUT_DATA_URI, self::SVG_OUTPUT_INLINE)
-                );
+                $writer = new PngWriter();
+                break;
         }
 
+        $qrCode = new EndroidQrCode($text);
+        $qrCode->setSize($size);
+        $qrCode->setMargin($margin);
+
+        $mode = new RoundBlockSizeModeShrink();
+
+        if (!empty($logo)) {
+            $logo = new Logo($logo, $logoResizeToWidth, $logoResizeToHeight);
+        } else {
+            $logo = null;
+        }
+
+        $qrCode->setRoundBlockSizeMode($mode);
+
+        $label = Label::create($label);
+        if (!empty($labelFontPath)) {
+            $labelFont = new Font($labelFontPath);
+            $label->setFont($labelFont);
+        }
+
+        $result =  $writer->write($qrCode, $logo, $label);
+
+        return $result->getDataUri();
     }
 }
